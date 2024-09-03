9103b67fdeb1249f182a7b6f9befe839d50b229d
Akshit Gupta <akshu.24gupta@gmail.com>
2024-09-03 19:48:39 +0530

    added github link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+import { ExternalLink } from "lucide-react";
+
 const Footer = () => {
   return (
     <div className="flex gap-2 text-gray-600 text-[12px] font-normal mb-4">
@@ -15,6 +17,17 @@ const Footer = () => {
         className="hover:text-gray-900"
       >
         Twitter
+      </a>
+      <div className="h-full w-[0.4px] bg-gray-400  "></div>
+      <a
+        href="https://github.com/Ak2407/v0.1"
+        target="_blank"
+        className="hover:text-gray-900"
+      >
+        <div className="flex items-center gap-1">
+          <h1>Github</h1>
+          <ExternalLink className="w-3 h-3 " />
+        </div>
       </a>
     </div>
   );
